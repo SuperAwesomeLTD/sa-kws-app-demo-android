295b8f5d0de80c081f0aaef1ea48a24a683488b7
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-06-15 14:45:14 +0100

    Upadte final piece of interface

--- app/src/main/java/superawesome/tv/kwsappdemo/FeatureFragment.java
+++ app/src/main/java/superawesome/tv/kwsappdemo/FeatureFragment.java
@@ -1,16 +1,30 @@
 package superawesome.tv.kwsappdemo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by gabriel.coman on 15/06/16.
  */
 public class FeatureFragment extends Fragment {
+
+    // constants
+    private final String AUTHURL = "https://developers.superawesome.tv/extdocs/sa-kws-android-sdk/html/index.html";
+    private final String NOTIFURL = "https://developers.superawesome.tv/extdocs/sa-kws-android-sdk/html/index.html";
+
+    // buttons
+    private Button authAction;
+    private Button authDocs;
+    private Button notifEnable;
+    private Button notifSend;
+    private Button notifDocs;
 
     // constructor
     public FeatureFragment () {
@@ -26,6 +40,49 @@ public class FeatureFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_feature, container, false);
+
+        authAction = (Button) view.findViewById(R.id.authAction);
+        authAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        authDocs = (Button) view.findViewById(R.id.authDocs);
+        authDocs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(AUTHURL));
+                getContext().startActivity(browserIntent);
+            }
+        });
+
+        notifEnable = (Button) view.findViewById(R.id.notifEnable);
+        notifEnable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        notifSend = (Button) view.findViewById(R.id.notifSend);
+        notifSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        notifDocs = (Button) view.findViewById(R.id.notifDocs);
+        notifDocs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(NOTIFURL));
+                getContext().startActivity(browserIntent);
+            }
+        });
+
         return view;
     }
 }
